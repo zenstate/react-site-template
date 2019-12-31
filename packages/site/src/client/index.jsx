2b90e56988f267e0547fff89ea07c1596a4a3893
liam-mclaughlin <liam-mclaughlin@users.noreply.github.com>
2019-12-31 18:53:55 +0000

    fixed style loading dependency

--- packages/site/src/client/index.jsx
+++ packages/site/src/client/index.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './scss/index.scss';
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root'),
